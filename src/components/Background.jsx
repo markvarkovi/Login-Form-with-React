b460054e7edcafe9c35980c36ea881e65bf0b0d3
Varkovi Mark <vmark33@gmail.com>
2024-02-13 18:22:45 +0100

    dynamic background, not working version yet

---
+++ src/components/Background.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import "./Background.css";
+
+function DynamicBackground() {
+  const [timeOfDay, setTimeOfDay] = useState("");
+
+  useEffect(() => {
+    let hour = new Date().getHours();
+    if (hour >= 0 && hour < 12) {
+      setTimeOfDay("morning");
+    } else if (hour >= 12 && hour < 18) {
+      setTimeOfDay("afternoon");
+    } else {
+      setTimeOfDay("night");
+    }
+  }, []);
+
+  return (
+    <>
+      {timeOfDay === "morning" ? (
+        <div className="morning-bg">
+          <h2>Good Morning!</h2>
+        </div>
+      ) : timeOfDay === "afternoon" ? (
+        <div className="afternoon-bg">
+            <h2>Good Afternoon!</h2>
+        </div>
+      ) : (
+        <div className="evening-bg">
+            <h2>Good Evening!</h2>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default DynamicBackground;
